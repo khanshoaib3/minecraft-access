2d235f8b4c551ddd3fcb5bfa73b984bf8c0eea64
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2022-08-31 18:10:56 +0530

    Added mouse click simulation for linux

--- src/main/java/com/github/khanshoaib3/minecraft_access/MainClass.java
+++ src/main/java/com/github/khanshoaib3/minecraft_access/MainClass.java
@@ -1,7 +1,10 @@
 package com.github.khanshoaib3.minecraft_access;
 
+import com.github.khanshoaib3.minecraft_access.features.MenuFix;
 import com.github.khanshoaib3.minecraft_access.screen_reader.ScreenReaderController;
 import com.github.khanshoaib3.minecraft_access.screen_reader.ScreenReaderInterface;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +13,8 @@ import net.fabricmc.api.ModInitializer;
 public class MainClass implements ModInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger("minecraft_access");
     private static ScreenReaderInterface screenReader = null;
+
+    public static boolean debugMode = true; // TODO add option to toggle this
 
     @Override
     public void onInitialize() {
@@ -20,6 +25,9 @@ public class MainClass implements ModInitializer {
         if (getScreenReader() != null && getScreenReader().isInitialized())
             getScreenReader().say(msg, true);
 
+        System.setProperty("java.awt.headless","false");
+        ClientTickEvents.END_CLIENT_TICK.register(this::clientTickEventsMethod);
+
         // This executes when minecraft closes
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (getScreenReader() != null && getScreenReader().isInitialized())
@@ -27,9 +35,17 @@ public class MainClass implements ModInitializer {
         }, "Shutdown-thread"));
     }
 
+    /**
+     * This method gets called at the end of every tick
+     *
+     * @param minecraftClient The current minecraft client object
+     */
+    private void clientTickEventsMethod(MinecraftClient minecraftClient) {
+        MenuFix.update(minecraftClient);
+    }
+
     public static void infoLog(String msg) {
-        //TODO add debug enabling/disabling logic here
-        LOGGER.info(msg);
+        if (debugMode) LOGGER.info(msg);
     }
 
     public static void errorLog(String msg) {
---
+++ src/main/java/com/github/khanshoaib3/minecraft_access/features/MenuFix.java
@@ -0,0 +1,70 @@
+package com.github.khanshoaib3.minecraft_access.features;
+
+import com.github.khanshoaib3.minecraft_access.MainClass;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MenuFix {
+    private static String prevScreenTitle = "";
+    private static final List<String> menuList = new ArrayList<>(){{
+        add("title screen");
+        add("options");
+        add("controls");
+        add("online options");
+        add("skin customization");
+        add("music & sound options");
+        add("video settings");
+        add("language...");
+        add("chat settings...");
+        add("select resource packs");
+        add("accessibility settings...");
+        add("mouse settings");
+        add("key binds");
+    }};
+
+    public static void update(MinecraftClient minecraftClient) {
+        if (minecraftClient.currentScreen == null)
+            return;
+
+        try {
+            String title = minecraftClient.currentScreen.getTitle().getString();
+            if (menuList.contains(title.toLowerCase())) {
+
+                if (!prevScreenTitle.equalsIgnoreCase(title)) {
+                    MainClass.infoLog(title + " opened, now moving the mouse cursor.");
+                    moveMouseCursor(minecraftClient);
+                    prevScreenTitle = title;
+                }
+
+                boolean isRPressed = (InputUtil.isKeyPressed(minecraftClient.getWindow().getHandle(),
+                        InputUtil.fromTranslationKey("key.keyboard.r").getCode()));
+                if (isRPressed)
+                    moveMouseCursor(minecraftClient);
+            }
+        } catch (Exception e) {
+            MainClass.errorLog("Error encountered while running the menu fix feature\n");
+            e.printStackTrace();
+        }
+    }
+
+    private static void moveMouseCursor(MinecraftClient minecraftClient) {
+        try {
+            int movePosX = 1, movePosY = 1;
+            if (!minecraftClient.options.getFullscreen().getValue()) {
+                movePosX += minecraftClient.getWindow().getX();
+                movePosY += minecraftClient.getWindow().getY();
+            }
+
+            MainClass.infoLog("Moving the mouse to x:" + movePosX + " y:" + movePosY);
+            Runtime.getRuntime().exec("xdotool mousemove " + movePosX + " " + movePosY);
+            Runtime.getRuntime().exec("xdotool click 1");
+        } catch (Exception e) {
+            MainClass.errorLog("Error encountered while moving the mouse for the menu fix feature\n");
+            e.printStackTrace();
+        }
+    }
+}
