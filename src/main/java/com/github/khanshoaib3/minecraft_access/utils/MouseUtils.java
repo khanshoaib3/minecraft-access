9122ffdcc0cfd7f2426d2e6d731f9564f25225af
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2022-09-03 13:56:32 +0530

    Added mouse sim functions for windows

--- src/main/java/com/github/khanshoaib3/minecraft_access/utils/MouseUtils.java
+++ src/main/java/com/github/khanshoaib3/minecraft_access/utils/MouseUtils.java
@@ -1,11 +1,13 @@
 package com.github.khanshoaib3.minecraft_access.utils;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
 import net.minecraft.client.MinecraftClient;
 
-import java.io.IOException;
-
 public class MouseUtils {
+    private static user32dllInterface mainInterface = null;
+
     public static void moveAndLeftClick(int x, int y) {
         move(x, y);
         leftClick();
@@ -20,11 +22,17 @@ public class MouseUtils {
         try {
             MainClass.infoLog("Moving mouse to x:%d y:%d".formatted(x, y));
 
-            if (OsUtils.isLinux())
+            if (OsUtils.isLinux()) {
                 Runtime.getRuntime().exec("xdotool mousemove %d %d".formatted(x, y));
-            else if (OsUtils.isWindows()) {
             }
-        } catch (IOException e) {
+
+            if (OsUtils.isWindows()) {
+                if (mainInterface == null) initializeUser32dll();
+
+                if (!mainInterface.SetCursorPos(x, y))
+                    MainClass.errorLog("Error encountered on moving mouse.");
+            }
+        } catch (Exception e) {
             MainClass.errorLog("Error encountered on moving mouse.");
             e.printStackTrace();
         }
@@ -36,11 +44,17 @@ public class MouseUtils {
             int y = (int) MinecraftClient.getInstance().mouse.getY();
             MainClass.infoLog("Performing left click at x:%d y:%d".formatted(x, y));
 
-            if (OsUtils.isLinux())
+            if (OsUtils.isLinux()) {
                 Runtime.getRuntime().exec("xdotool click 1");
-            else if (OsUtils.isWindows()) {
             }
-        } catch (IOException e) {
+
+            if (OsUtils.isWindows()) {
+                if (mainInterface == null) initializeUser32dll();
+
+                mainInterface.mouse_event(MouseEventFlags.LEFTDOWN.getValue(), 0, 0, 0, 0);
+                mainInterface.mouse_event(MouseEventFlags.LEFTUP.getValue(), 0, 0, 0, 0);
+            }
+        } catch (Exception e) {
             MainClass.errorLog("Error encountered on performing left mouse click.");
             e.printStackTrace();
         }
@@ -53,13 +67,55 @@ public class MouseUtils {
             int y = (int) MinecraftClient.getInstance().mouse.getY();
             MainClass.infoLog("Performing right click at x:%d y:%d".formatted(x, y));
 
-            if (OsUtils.isLinux())
+            if (OsUtils.isLinux()) {
                 Runtime.getRuntime().exec("xdotool click 3");
-            else if (OsUtils.isWindows()) {
             }
-        } catch (IOException e) {
+
+            if (OsUtils.isWindows()) {
+                if (mainInterface == null) initializeUser32dll();
+
+                mainInterface.mouse_event(MouseEventFlags.RIGHTDOWN.getValue(), 0, 0, 0, 0);
+                mainInterface.mouse_event(MouseEventFlags.RIGHTUP.getValue(), 0, 0, 0, 0);
+            }
+        } catch (Exception e) {
             MainClass.errorLog("Error encountered on performing right mouse click.");
             e.printStackTrace();
+        }
+    }
+
+    private static void initializeUser32dll() {
+        try {
+            mainInterface = Native.load("User32.dll", user32dllInterface.class);
+        } catch (Exception e) {
+            MainClass.errorLog("Error encountered while initializing User32.dll");
+            e.printStackTrace();
+        }
+    }
+
+    private interface user32dllInterface extends Library {
+        boolean SetCursorPos(int x, int y);
+
+        void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+    }
+
+    private enum MouseEventFlags {
+        LEFTDOWN(0x00000002),
+        LEFTUP(0x00000004),
+        MIDDLEDOWN(0x00000020),
+        MIDDLEUP(0x00000040),
+        MOVE(0x00000001),
+        ABSOLUTE(0x00008000),
+        RIGHTDOWN(0x00000008),
+        RIGHTUP(0x00000010);
+
+        private final int value;
+
+        MouseEventFlags(final int newValue) {
+            value = newValue;
+        }
+
+        public int getValue() {
+            return value;
         }
     }
 }
