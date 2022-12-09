88f37d269b279ed4344ff0abd7dbb37f87848af4
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2022-12-09 16:40:34 +0530

    Removed unnecessary narration messages
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/ScreenMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/ScreenMixin.java
@@ -1,20 +1,62 @@
 package com.github.khanshoaib3.minecraft_access.mixin;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.screen.narration.NarrationPart;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(Screen.class)
-public class ScreenMixin {
+public abstract class ScreenMixin {
+    @Shadow public abstract Text getNarratedTitle();
+
+    @Shadow
+    @Nullable
+    public static Screen.SelectedElementNarrationData findSelectedElementData(List<? extends Selectable> selectables, @Nullable Selectable selectable) {
+        return null;
+    }
+
+    @Shadow @Nullable private Selectable selected;
+
+    @Shadow @Final private List<Selectable> selectables;
+
+    @Shadow protected abstract void addElementNarrations(NarrationMessageBuilder builder);
+
+    @Inject(at = @At("HEAD"), method = "addScreenNarrations", cancellable = true)
+    private void addScreenNarrationsHead(NarrationMessageBuilder builder, CallbackInfo ci){
+        builder.put(NarrationPart.TITLE, this.getNarratedTitle());
+        this.addElementNarrations(builder);
+        ci.cancel();
+    }
+
     @Inject(at = @At("HEAD"), method = "addElementNarrations*", cancellable = true)
-    private void addElementNarrations(NarrationMessageBuilder builder, CallbackInfo callbackInfo) {
+    private void addElementNarrationsHead(NarrationMessageBuilder builder, CallbackInfo callbackInfo) {
         if (MinecraftClient.getInstance().currentScreen instanceof MerchantScreen) {
             callbackInfo.cancel();
         }
+
+        ImmutableList<Selectable> immutableList = (ImmutableList)this.selectables.stream().filter(Selectable::isNarratable).collect(ImmutableList.toImmutableList());
+        Screen.SelectedElementNarrationData selectedElementNarrationData = this.findSelectedElementData(immutableList, this.selected);
+        if (selectedElementNarrationData != null) {
+            if (selectedElementNarrationData.selectType.isFocused()) {
+                this.selected = selectedElementNarrationData.selectable;
+            }
+
+            selectedElementNarrationData.selectable.appendNarrations(builder.nextMessage());
+        }
+
+        callbackInfo.cancel();
     }
 }
