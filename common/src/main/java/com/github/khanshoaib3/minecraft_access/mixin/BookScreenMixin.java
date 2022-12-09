3d6e758602a07259fcfb468242d63e5d8fc69db2
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2022-12-09 16:54:22 +0530

    Added keybind (left alt + r) to repeat current page content
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/BookScreenMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/BookScreenMixin.java
@@ -3,6 +3,8 @@ package com.github.khanshoaib3.minecraft_access.mixin;
 import com.github.khanshoaib3.minecraft_access.MainClass;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.BookScreen;
+import net.minecraft.client.gui.widget.PageTurnWidget;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,14 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BookScreen.class)
 public class BookScreenMixin {
-    @Shadow private int pageIndex;
-    @Shadow private BookScreen.Contents contents;
+    @Shadow
+    private int pageIndex;
+    @Shadow
+    private BookScreen.Contents contents;
 
-    @Shadow private Text pageIndexText;
+    @Shadow
+    private Text pageIndexText;
+    @Shadow private PageTurnWidget nextPageButton;
+    @Shadow private PageTurnWidget previousPageButton;
     String previousContent = "";
 
     @Inject(at = @At("HEAD"), method = "render")
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo){
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo) {
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
         if (minecraftClient == null) return;
         if (minecraftClient.currentScreen == null) {
@@ -33,6 +40,17 @@ public class BookScreenMixin {
             return;
         }
 
+        boolean isLeftAltPressed = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(),
+                InputUtil.fromTranslationKey("key.keyboard.left.alt").getCode());
+        boolean isRPressed = (InputUtil.isKeyPressed(minecraftClient.getWindow().getHandle(),
+                InputUtil.fromTranslationKey("key.keyboard.r").getCode()));
+
+        // Repeat current page content and un-focus next and previous page buttons
+        if (isLeftAltPressed && isRPressed) {
+            if(this.nextPageButton.isFocused()) this.nextPageButton.changeFocus(false);
+            if(this.previousPageButton.isFocused()) this.previousPageButton.changeFocus(false);
+            previousContent = "";
+        }
 
         int pageIndex = this.pageIndex;
         if (pageIndex < 0 || pageIndex > this.contents.getPageCount())
