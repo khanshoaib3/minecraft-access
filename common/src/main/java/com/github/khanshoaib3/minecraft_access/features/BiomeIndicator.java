501eec14db91719295f9ba51e5a05fe2213233f1
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2022-12-18 12:48:42 +0530

    Fixed biome indicator
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/features/BiomeIndicator.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/BiomeIndicator.java
@@ -2,6 +2,9 @@ package com.github.khanshoaib3.minecraft_access.features;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.biome.Biome;
 
 /**
  * Narrates the name of the biome when entering a different biome.
@@ -20,23 +23,47 @@ public class BiomeIndicator {
             if (minecraftClient.world == null) return;
             if (minecraftClient.player == null) return;
 
-            /*// Identifier id = minecraftClient.world.getRegistryManager().get(Registry.BIOME_KEY).getId(minecraftClient.world.getBiome(minecraftClient.player.getBlockPos()).value()); // Pre 1.19.3
-            Identifier id = minecraftClient.world.getRegistryManager().get(Registries.BIOME_SOURCE.getEntry()).get(minecraftClient.world.getBiome(minecraftClient.player.getBlockPos()).getKey());
+            RegistryEntry<Biome> var27 = minecraftClient.world.getBiome(minecraftClient.player.getBlockPos());
+            String name = I18n.translate(getBiomeName(var27));
+
+            /* Pre 1.19.3
+            Identifier id = minecraftClient.world.getRegistryManager().get(Registry.BIOME_KEY).getId(minecraftClient.world.getBiome(minecraftClient.player.getBlockPos()).value()); // Pre 1.19.3
             if (id == null) {
                 MainClass.errorLog("\nUnable to detect biome!!\n");
                 return;
             }
-
             String name = I18n.translate("biome." + id.getNamespace() + "." + id.getPath());
+            */
 
             if (!previousBiome.equalsIgnoreCase(name)) {
                 previousBiome = name;
                 MainClass.speakWithNarrator(I18n.translate("minecraft_access.other.biome", name), true);
-            }*/
+            }
 
         } catch (Exception e) {
             MainClass.errorLog("An error occurred while narrating biome.");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Gets the biome name from registry entry
+     * @param biome the biome's registry entry
+     * @return the biome's name
+     */
+    private static String getBiomeName(RegistryEntry<Biome> biome) {
+        return I18n.translate(getBiomeTranslationKey(biome));
+    }
+
+    /**
+     * Gets the biome translation key from registry entry
+     * @param biome the biome's registry entry
+     * @return the biome's translation key
+     */
+    private static String getBiomeTranslationKey(RegistryEntry<Biome> biome) {
+        return biome.getKeyOrValue().map(
+            (biomeKey) -> "biome." + biomeKey.getValue().getNamespace() + "." + biomeKey.getValue().getPath(),
+            (biomeValue) -> "[unregistered " + biomeValue + "]" // For unregistered biome
+        );
     }
 }
