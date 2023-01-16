36643dfca8de4d6d7cf3818ad20a9d05b1d9bde8
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-01-16 20:50:47 +0530

    Organized code
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/features/narrator_menu/NarratorMenuGUI.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/narrator_menu/NarratorMenuGUI.java
@@ -1,6 +1,7 @@
 package com.github.khanshoaib3.minecraft_access.features.narrator_menu;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.resource.language.I18n;
@@ -8,56 +9,57 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class NarratorMenuGUI extends Screen {
+    int centerX;
+    int buttonHeight;
+    int marginY;
+    int calculatedYPosition;
+
     public NarratorMenuGUI(String title) {
         super(Text.of(I18n.translate("minecraft_access.gui.screen." + title)));
     }
 
     @Override
     protected void init() {
-        int centerX = this.width / 2;
-        int buttonHeight = 20;
-        int startY = this.height / 6;
+        this.centerX = this.width / 2;
+        this.buttonHeight = 20;
+        this.marginY = buttonHeight + buttonHeight / 4;
+        this.calculatedYPosition = this.height / 6 - marginY; // Starting Y position (marginY will again be added in buildButtonWidget() so it is subtracted here to equate)
 
-        ButtonWidget blockAndFluidTargetInformationButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_info"),
-                (button) -> NarratorMenu.getBlockAndFluidTargetInformation()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_info"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_info"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget blockAndFluidTargetPositionButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_position"),
-                (button) -> NarratorMenu.getBlockAndFluidTargetPosition()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_position"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_position"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget lightLevelButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.light_level"),
-                (button) -> NarratorMenu.getLightLevel()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.light_level"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.light_level"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget findWaterButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.find_water"),
-                (button) -> MainClass.fluidDetector.findClosestWaterSource()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.find_water"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.find_water"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget findLavaButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.find_lava"),
-                (button) -> MainClass.fluidDetector.findClosestLavaSource()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.find_lava"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.find_lava"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget biomeButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.biome"),
-                (button) -> NarratorMenu.getBiome()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.biome"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.biome"))) + 35, buttonHeight).build();
-        startY += buttonHeight + buttonHeight / 2;
-
-        ButtonWidget xpButton = ButtonWidget.builder(Text.translatable("minecraft_access.narrator_menu.gui.button.xp"),
-                (button) -> NarratorMenu.getXP()).dimensions(centerX - (this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.xp"))) + 35) / 2, startY, this.textRenderer.getWidth(I18n.translate(("minecraft_access.narrator_menu.gui.button.xp"))) + 35, buttonHeight).build();
-
+        ButtonWidget blockAndFluidTargetInformationButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_info", (button) -> NarratorMenu.getBlockAndFluidTargetInformation());
         this.addDrawableChild(blockAndFluidTargetInformationButton);
+
+        ButtonWidget blockAndFluidTargetPositionButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.block_and_fluid_target_position", (button) -> NarratorMenu.getBlockAndFluidTargetPosition());
         this.addDrawableChild(blockAndFluidTargetPositionButton);
+
+        ButtonWidget lightLevelButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.light_level", (button) -> NarratorMenu.getLightLevel());
         this.addDrawableChild(lightLevelButton);
+
+        ButtonWidget findWaterButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.find_water", (button) -> MainClass.fluidDetector.findClosestWaterSource());
         this.addDrawableChild(findWaterButton);
+
+        ButtonWidget findLavaButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.find_lava", (button) -> MainClass.fluidDetector.findClosestLavaSource());
         this.addDrawableChild(findLavaButton);
+
+        ButtonWidget biomeButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.biome", (button) -> NarratorMenu.getBiome());
         this.addDrawableChild(biomeButton);
+
+        ButtonWidget xpButton = this.buildButtonWidget("minecraft_access.narrator_menu.gui.button.xp", (button) -> NarratorMenu.getXP());
         this.addDrawableChild(xpButton);
+    }
+
+    private ButtonWidget buildButtonWidget(String translationKey, ButtonWidget.PressAction pressAction) {
+        int calculatedButtonWidth = this.textRenderer.getWidth(I18n.translate((translationKey))) + 35;
+        calculatedYPosition += marginY;
+
+        return ButtonWidget.builder(Text.translatable(translationKey), pressAction)
+                .dimensions(centerX - calculatedButtonWidth / 2, calculatedYPosition, calculatedButtonWidth, buttonHeight)
+                .build();
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
-        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 16777215);
+        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 16777215);
         super.render(matrices, mouseX, mouseY, delta);
     }
 }
