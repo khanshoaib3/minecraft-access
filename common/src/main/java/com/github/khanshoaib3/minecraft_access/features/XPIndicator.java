dca5d327e45cd9a8abf9bf1e6263572d079d15d3
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-01-22 14:35:40 +0530

    Added xp indicator
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

---
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/XPIndicator.java
@@ -0,0 +1,37 @@
+package com.github.khanshoaib3.minecraft_access.features;
+
+import com.github.khanshoaib3.minecraft_access.MainClass;
+import com.github.khanshoaib3.minecraft_access.utils.ClientPlayerEntityUtils;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.language.I18n;
+
+public class XPIndicator {
+    private int previousXPLevel;
+
+    public XPIndicator() {
+        previousXPLevel = -999;
+    }
+
+    public void update() {
+        try {
+            MinecraftClient minecraftClient = MinecraftClient.getInstance();
+            if (minecraftClient == null) return;
+            if (minecraftClient.world == null) return;
+            if (minecraftClient.player == null) return;
+            if (minecraftClient.currentScreen != null) return;
+
+            int currentXPLevel = ClientPlayerEntityUtils.getExperienceLevel();
+            if (previousXPLevel == currentXPLevel) return;
+
+            boolean increased = previousXPLevel < currentXPLevel;
+            previousXPLevel = currentXPLevel;
+
+            String toSpeak = (increased) ? I18n.translate("minecraft_access.xp_indicator.increased", currentXPLevel)
+                    : I18n.translate("minecraft_access.xp_indicator.decreased", currentXPLevel);
+            MainClass.speakWithNarrator(toSpeak, true);
+        } catch (Exception e) {
+            MainClass.errorLog("An error occurred in xp indicator.");
+            e.printStackTrace();
+        }
+    }
+}
