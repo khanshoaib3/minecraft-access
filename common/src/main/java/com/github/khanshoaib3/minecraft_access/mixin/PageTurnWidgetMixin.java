52fcac9c1cdd04bd2d1c4d6da6fa5221ec3bd23e
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-06-28 18:12:16 +0530

    Fixed a bug that crashes mc when opening a book edit screen #60
    Signed-off-by: Mohammad Shoaib Khan <shoaib.khan20@outlook.com>

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/PageTurnWidgetMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/PageTurnWidgetMixin.java
@@ -1,8 +1,8 @@
 package com.github.khanshoaib3.minecraft_access.mixin;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.PageTurnWidget;
 import net.minecraft.client.resource.language.I18n;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +16,7 @@ public class PageTurnWidgetMixin {
     @Shadow @Final private boolean isNextPageButton;
 
     @Inject(at = @At("HEAD"), method = "renderButton")
-    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo){
+    public void renderButton(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci){
         if(this.isNextPageButton)
             ((ClickableWidgetAccessor)this).setMessage(Text.literal(I18n.translate("minecraft_access.menus.book_screen.next_page_button_name")));
         else
