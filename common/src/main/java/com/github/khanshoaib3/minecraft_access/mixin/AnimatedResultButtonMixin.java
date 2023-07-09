6bb11981e04696763f8b52b95767bc22ad569e51
boholder <bottleholder@anche.no>
2023-07-09 21:05:07 +0800

    Fix speak AnimatedResultButton, close #33

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/AnimatedResultButtonMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/AnimatedResultButtonMixin.java
@@ -1,25 +1,57 @@
 package com.github.khanshoaib3.minecraft_access.mixin;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
+import com.github.khanshoaib3.minecraft_access.utils.MouseUtils;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.screen.recipebook.AnimatedResultButton;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AnimatedResultButton.class)
 public class AnimatedResultButtonMixin {
-//    @Inject(at = @At("HEAD"), method = "appendNarrations", cancellable = true) // Pre 1.19.3
+    @Unique
+    boolean minecraft_access$vibratingFlag = false;
+
+    @Unique
+    String minecraft_access$previousItemName = "";
+
+    //    @Inject(at = @At("HEAD"), method = "appendNarrations", cancellable = true) // Pre 1.19.3
     @Inject(at = @At("HEAD"), method = "appendClickableNarrations", cancellable = true) // From 1.19.3
     private void appendNarrations(NarrationMessageBuilder builder, CallbackInfo callbackInfo) {
-        //TODO re-check if it still works
         ItemStack itemStack = ((AnimatedResultButtonAccessor) this).callGetResults().get(((AnimatedResultButtonAccessor) this).getCurrentResultIndex()).getOutput();
-        String toSpeak = "%s %d %s".formatted(((AnimatedResultButtonAccessor) this).getResultCollection().hasCraftableRecipes() ? "Craftable" : "Not craftable", itemStack.getCount(), itemStack.getName().getString());
+        String itemName = itemStack.getName().getString();
 
-        MainClass.speakWithNarrator(toSpeak, true);
+        if (!itemName.equalsIgnoreCase(minecraft_access$previousItemName)) {
+            String craftable = ((AnimatedResultButtonAccessor) this).getResultCollection().hasCraftableRecipes() ? "craftable" : "not_craftable";
+            craftable = I18n.translate("minecraft_access.other." + craftable);
+            String toSpeak = "%s %d %s".formatted(craftable, itemStack.getCount(), itemName);
+            MainClass.speakWithNarrator(toSpeak, true);
+            minecraft_access$previousItemName = itemName;
+        }
 
+        minecraft_access$shakeTheMouse();
         callbackInfo.cancel();
+    }
+
+    /**
+     * It seems the "appendNarrations" will be invoked after every mouse moving.
+     * Keep moving the mouse to trigger this method to read different items in same animated button.
+     * It's not a solution that gets to the root of the problem, but I think it's simpler and more stable.
+     * This method doesn't affect slot moving inside recipe book group.
+     */
+    @Unique
+    private void minecraft_access$shakeTheMouse() {
+        // the width and height of one animated button are both 25.
+        int offset = this.minecraft_access$vibratingFlag ? 12 : 13;
+        int x = ((ClickableWidgetAccessor) this).callGetX() + offset;
+        int y = ((ClickableWidgetAccessor) this).callGetY() + offset;
+        MouseUtils.Coordinates p = MouseUtils.calcRealPositionOfWidget(x, y);
+        MouseUtils.move(p.x(), p.y());
+        this.minecraft_access$vibratingFlag = !this.minecraft_access$vibratingFlag;
     }
 }
