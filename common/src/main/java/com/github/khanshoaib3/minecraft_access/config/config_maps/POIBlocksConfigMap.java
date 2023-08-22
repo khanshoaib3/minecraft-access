73dbcb70cdec2be1c6fb151e3640fe1bb89e7de4
boholder <bottleholder@anche.no>
2023-08-22 18:27:39 +0800

    Make POIBlocksConfigMap singleton

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/config/config_maps/POIBlocksConfigMap.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/config/config_maps/POIBlocksConfigMap.java
@@ -1,8 +1,12 @@
 package com.github.khanshoaib3.minecraft_access.config.config_maps;
 
+import com.github.khanshoaib3.minecraft_access.config.Config;
 import com.google.gson.annotations.SerializedName;
 
 public class POIBlocksConfigMap {
+
+    private static POIBlocksConfigMap instance;
+
     @SerializedName("Enabled")
     private boolean enabled;
     @SerializedName("Detect Fluid Blocks")
@@ -17,6 +21,29 @@ public class POIBlocksConfigMap {
     private boolean playSoundForOtherBlocks;
     @SerializedName("Delay (in milliseconds)")
     private int delay;
+
+    public static POIBlocksConfigMap getInstance() {
+        if (instance == null) Config.getInstance().loadConfig();
+        return instance;
+    }
+
+    public static void setInstance(POIBlocksConfigMap map) {
+        instance = map;
+    }
+
+    public static POIBlocksConfigMap buildDefault() {
+        POIBlocksConfigMap m = new POIBlocksConfigMap();
+        m.setEnabled(true);
+        m.setDetectFluidBlocks(true);
+        m.setRange(6);
+        m.setPlaySound(true);
+        m.setVolume(0.25f);
+        m.setPlaySoundForOtherBlocks(false);
+        m.setDelay(3000);
+
+        setInstance(m);
+        return m;
+    }
 
     public boolean isEnabled() {
         return enabled;
