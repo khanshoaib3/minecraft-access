9a5015bfa4daa2caf53d61ceb63bcfdf644f6f48
boholder <bottleholder@anche.no>
2023-11-12 22:32:41 +0800

    Speak cursor hover over text when editing sign

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/SelectionManagerMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/SelectionManagerMixin.java
@@ -1,8 +1,12 @@
 package com.github.khanshoaib3.minecraft_access.mixin;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
+import net.minecraft.client.font.TextHandler;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.SelectionManager;
 import net.minecraft.util.Util;
+import org.apache.logging.log4j.util.Strings;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,11 +28,66 @@ public abstract class SelectionManagerMixin {
     @Shadow
     private int selectionEnd;
 
-    @Shadow protected abstract String getSelectedText(String string);
+    @Shadow
+    protected abstract String getSelectedText(String string);
 
     @Inject(at = @At("TAIL"), method = "putCursorAtEnd")
     public void speakTextOfSwitchedLine(CallbackInfo ci) {
         MainClass.speakWithNarratorIfNotEmpty(this.stringGetter.get(), true);
+    }
+
+    @Inject(at = @At("HEAD"), method = "handleSpecialKey")
+    private void speakCursorHoverOverText(int keyCode, CallbackInfoReturnable<Boolean> cir) {
+        // is selecting, let the selecting text speaking method do the job instead
+        if (Screen.hasShiftDown()) {
+            return;
+        }
+
+        switch (keyCode) {
+            case GLFW.GLFW_KEY_LEFT: {
+                if (Screen.hasControlDown()) {
+                    String hoveredText = this.getCursorHoveredOverText(getCursorPosByWordsWithOffset(-1));
+                    MainClass.speakWithNarratorIfNotEmpty(hoveredText, true);
+                } else {
+                    String hoveredText = this.getCursorHoveredOverText(getCursorPosWithOffset(-1));
+                    MainClass.speakWithNarratorIfNotEmpty(hoveredText, true);
+                }
+                return;
+            }
+            case GLFW.GLFW_KEY_RIGHT: {
+                if (Screen.hasControlDown()) {
+                    String hoveredText = this.getCursorHoveredOverText(getCursorPosByWordsWithOffset(1));
+                    MainClass.speakWithNarratorIfNotEmpty(hoveredText, true);
+                } else {
+                    String hoveredText = this.getCursorHoveredOverText(getCursorPosWithOffset(1));
+                    MainClass.speakWithNarratorIfNotEmpty(hoveredText, true);
+                }
+                return;
+            }
+            case GLFW.GLFW_KEY_HOME: {
+                String text = this.stringGetter.get();
+                if (Strings.isNotEmpty(text)) {
+                    MainClass.speakWithNarrator(text.substring(0, 1), true);
+                }
+                return;
+            }
+            case GLFW.GLFW_KEY_END: {
+                String text = this.stringGetter.get();
+                if (Strings.isNotEmpty(text)) {
+                    MainClass.speakWithNarrator(text.substring(text.length() - 1), true);
+                }
+            }
+        }
+    }
+
+    @Unique
+    private int getCursorPosByWordsWithOffset(int offset) {
+        return TextHandler.moveCursorByWords(this.stringGetter.get(), offset, this.selectionStart, true);
+    }
+
+    @Unique
+    private int getCursorPosWithOffset(int offset) {
+        return Util.moveCursor(this.stringGetter.get(), this.selectionStart, offset);
     }
 
     @Inject(at = @At("RETURN"), method = "handleSpecialKey")
