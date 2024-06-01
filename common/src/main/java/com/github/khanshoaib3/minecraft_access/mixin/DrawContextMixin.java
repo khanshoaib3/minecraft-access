9bc0bc9df236f9946537ba539ad9c1d2ba415328
boholder <bottleholder@anche.no>
2024-06-01 18:26:48 +0800

    feat: add two more interception points for speaking hovered tooltip
    close #281

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/DrawContextMixin.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/mixin/DrawContextMixin.java
@@ -2,10 +2,11 @@ package com.github.khanshoaib3.minecraft_access.mixin;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
 import com.github.khanshoaib3.minecraft_access.config.config_maps.InventoryControlsConfigMap;
-import net.minecraft.client.MinecraftClient;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.item.TooltipData;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,25 +19,43 @@ import java.util.Optional;
 
 /**
  * Speak hovered tooltip when Inventory Controls is disabled.
+ * Need to get text before they turned into "OrderedText" or "TooltipComponent"
+ * (can't easily extract text from these types).
+ * So we intercept every "drawTooltip" invoke that in "drawHoverEvent" method.
  */
 @Mixin(DrawContext.class)
 public class DrawContextMixin {
-    @Unique private static String minecraft_access$previousTooltipText = "";
+    @Unique
+    private static String minecraft_access$previousTooltipText = "";
 
     @Inject(at = @At("HEAD"), method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;II)V")
     private void speakHoveredTooltip(TextRenderer textRenderer, Text text, int x, int y, CallbackInfo ci) {
-        if (MinecraftClient.getInstance() == null) return;
-        if (MinecraftClient.getInstance().currentScreen == null) return;
         if (InventoryControlsConfigMap.getInstance().isEnabled()) return;
-
         minecraft_access$checkAndSpeak(text.getString());
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     @Inject(at = @At("HEAD"), method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V")
     private void speakHoveredTooltip2(TextRenderer textRenderer, List<Text> text, Optional<TooltipData> data, int x, int y, CallbackInfo ci) {
-        if (MinecraftClient.getInstance() == null) return;
-        if (MinecraftClient.getInstance().currentScreen == null) return;
+        if (InventoryControlsConfigMap.getInstance().isEnabled()) return;
+        minecraft_access$speakTextList(text);
+    }
+
+    @Inject(at = @At("HEAD"), method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;II)V")
+    private void speakHoveredTooltip3(TextRenderer textRenderer, List<Text> text, int x, int y, CallbackInfo ci) {
+        if (InventoryControlsConfigMap.getInstance().isEnabled()) return;
+        minecraft_access$speakTextList(text);
+    }
+
+    @Inject(method = "drawHoverEvent", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/DrawContext;drawOrderedTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;II)V"))
+    private void speakHoveredTooltip4(TextRenderer textRenderer, Style style, int x, int y, CallbackInfo ci, @Local Text text) {
+        if (InventoryControlsConfigMap.getInstance().isEnabled()) return;
+        minecraft_access$checkAndSpeak(text.getString());
+    }
+
+    @Unique
+    private static void minecraft_access$speakTextList(List<Text> text) {
         if (InventoryControlsConfigMap.getInstance().isEnabled()) return;
 
         StringBuilder toSpeak = new StringBuilder();
