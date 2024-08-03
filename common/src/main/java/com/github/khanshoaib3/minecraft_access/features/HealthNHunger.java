ac12c3fad39d79afba3bc6deb3307fd72a3c6fe0
boholder <bottleholder@anche.no>
2024-08-03 18:05:49 +0800

    feat: set key interval of health-and-hunger to 3s

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/features/HealthNHunger.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/HealthNHunger.java
@@ -2,6 +2,9 @@ package com.github.khanshoaib3.minecraft_access.features;
 
 import com.github.khanshoaib3.minecraft_access.MainClass;
 import com.github.khanshoaib3.minecraft_access.utils.KeyBindingsHandler;
+import com.github.khanshoaib3.minecraft_access.utils.condition.Interval;
+import com.github.khanshoaib3.minecraft_access.utils.condition.IntervalKeystroke;
+import com.github.khanshoaib3.minecraft_access.utils.condition.Keystroke;
 import com.github.khanshoaib3.minecraft_access.utils.system.KeyUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.minecraft.client.MinecraftClient;
@@ -13,6 +16,12 @@ import net.minecraft.client.resource.language.I18n;
  */
 @Slf4j
 public class HealthNHunger {
+    IntervalKeystroke narrationKey = new IntervalKeystroke(
+            () -> KeyUtils.isAnyPressed(KeyBindingsHandler.getInstance().healthNHungerNarrationKey),
+            Keystroke.TriggeredAt.PRESSED,
+            // 3s interval
+            Interval.inMilliseconds(3000));
+
     public void update() {
         try {
             MinecraftClient minecraftClient = MinecraftClient.getInstance();
@@ -20,17 +29,16 @@ public class HealthNHunger {
             if (minecraftClient.player == null) return;
             if (minecraftClient.currentScreen != null) return;
 
-            boolean isHealthNHungerNarrationKeyPressed = KeyUtils.isAnyPressed(KeyBindingsHandler.getInstance().healthNHungerNarrationKey);
-            if (!isHealthNHungerNarrationKeyPressed) return;
+            if (narrationKey.canBeTriggered()) {
+                double health = minecraftClient.player.getHealth();
+                double hunger = minecraftClient.player.getHungerManager().getFoodLevel();
+                health = (double) Math.round((health / 2) * 10) / 10;
+                hunger = (double) Math.round((hunger / 2) * 10) / 10;
+                String toSpeak = I18n.translate("minecraft_access.healthHunger.format", health, hunger);
+                MainClass.speakWithNarrator(toSpeak, true);
+            }
+            narrationKey.updateStateForNextTick();
 
-            double health = minecraftClient.player.getHealth();
-            double hunger = minecraftClient.player.getHungerManager().getFoodLevel();
-
-            health = (double) Math.round((health / 2) * 10) / 10;
-            hunger = (double) Math.round((hunger / 2) * 10) / 10;
-
-            String toSpeak = I18n.translate("minecraft_access.healthHunger.format", health, hunger);
-            MainClass.speakWithNarrator(toSpeak, true);
         } catch (Exception e) {
             log.error("An error occurred in HealthNHunger.", e);
         }
