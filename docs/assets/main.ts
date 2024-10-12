3159fe10fedda28b5a1b1d59a766fee40e260a97
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2024-10-12 13:21:09 +1100

    Added table of contents

--- docs/assets/main.ts
+++ docs/assets/main.ts
@@ -8,3 +8,19 @@ document.querySelectorAll(".toggle[aria-controls]").forEach(el => {
         el.setAttribute("aria-checked", (!collapsed).toString());
     })
 });
+
+const headings = Array.from(document.querySelectorAll(".content h2, .content h3")).reverse() as HTMLElement[];
+const toc = document.getElementsByClassName("table-of-contents")[0];
+const content = document.getElementsByClassName("content")[0];
+function onScroll() {
+    toc.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+    const heading = headings.filter(el => (el.offsetTop - el.clientHeight*3) < content.scrollTop)[0];
+    if (heading === undefined) {
+        return;
+    }
+    const tocLink = toc.querySelector(`a[href="#${heading.id}"]`) as HTMLElement;
+    tocLink.classList.add("active");
+    toc.scrollTo({top: tocLink.offsetTop - toc.clientHeight/2});
+}
+content.addEventListener("scroll", onScroll);
+onScroll();
